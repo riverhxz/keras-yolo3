5547fcfc7c3ecf6c27db5df8874dd655155a1a58
riverhxz <ravehun@qq.com>
2019-05-26 01:35:46 +0800

    correct summary

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_l
 from yolo3.utils import get_random_data
 from datetime import datetime
 from tqdm import *
-
+import os
 
 def _main():
     from train import get_classes, get_anchors
@@ -55,12 +55,13 @@ def _main():
 
     body = model_body(input_shape, anchors, num_classes, None,
                       freeze_body=2,
-                      weights_path='model_data/darknet53.weights.h5')
+                      init_weights='model_data/darknet53.weights.h5'
+                      ,last_save='model_data/tfmodel')
 
-    num_epoch = 10
+    num_epoch = 100
     optimizer = tf.keras.optimizers.Adam()
     tf.summary.experimental.set_step(0)
-    import os
+
 
     step = 0
     test_summary_writer = tf.summary.create_file_writer(os.path.join("logs/test/", datetime.now().strftime("%Y%m%d-%H%M%S")), )
@@ -104,7 +105,7 @@ def _main():
         val_loss = val_loss / num_val * batch_size
         print("val_loss:", val_loss)
 
-        # tf.saved_model.save(body, "model_data/tfmodel")
+        tf.saved_model.save(body, "model_data/tfmodel")
 
 
 
@@ -153,72 +154,8 @@ def get_anchors(anchors_path):
     anchors = [float(x) for x in anchors.split(',')]
     return np.array(anchors).reshape(-1, 2)
 
-
-def create_model(input_shape, anchors, num_classes, update_callback, load_pretrained=True, freeze_body=2,
-                 weights_path='model_data/yolo_weights.h5', input_tensor=None):
-    '''create the training model'''
-    K.clear_session()  # get a new session
-    image_input = input_tensor if input_tensor is not None else Input(shape=(None, None, 3))
-    h, w = input_shape
-    num_anchors = len(anchors)
-
-    y_true = [Input(shape=(h // {0: 32, 1: 16, 2: 8}[l], w // {0: 32, 1: 16, 2: 8}[l], \
-                           num_anchors // 3, num_classes + 5)) for l in range(3)]
-
-    model_body = yolo_body(image_input, num_anchors // 3, num_classes)
-    print('Create YOLOv3 model with {} anchors and {} classes.'.format(num_anchors, num_classes))
-
-    if load_pretrained:
-        model_body.load_weights(weights_path, by_name=True)
-        print('Load weights {}.'.format(weights_path))
-        if freeze_body in [1, 2]:
-            # Freeze darknet53 body or freeze all but 3 output layers.
-            num = (185, len(model_body.layers) - 3)[freeze_body - 1]
-            for i in range(num): model_body.layers[i].trainable = False
-            print('Freeze the first {} layers of total {} layers.'.format(num, len(model_body.layers)))
-
-    model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
-                        arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.5,
-                                   'update_callback': update_callback})(
-        [*model_body.output, *y_true])
-    model = Model([model_body.input, *y_true], model_loss)
-
-    return model
-
-
-def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, freeze_body=2,
-                      weights_path='model_data/tiny_yolo_weights.h5'):
-    '''create the training model, for Tiny YOLOv3'''
-    K.clear_session()  # get a new session
-    image_input = Input(shape=(None, None, 3))
-    h, w = input_shape
-    num_anchors = len(anchors)
-
-    y_true = [Input(shape=(h // {0: 32, 1: 16}[l], w // {0: 32, 1: 16}[l], \
-                           num_anchors // 2, num_classes + 5)) for l in range(2)]
-
-    model_body = tiny_yolo_body(image_input, num_anchors // 2, num_classes)
-    print('Create Tiny YOLOv3 model with {} anchors and {} classes.'.format(num_anchors, num_classes))
-
-    if load_pretrained:
-        model_body.load_weights(weights_path, by_name=True, skip_mismatch=True)
-        print('Load weights {}.'.format(weights_path))
-        if freeze_body in [1, 2]:
-            # Freeze the darknet body or freeze all but 2 output layers.
-            num = (20, len(model_body.layers) - 2)[freeze_body - 1]
-            for i in range(num): model_body.layers[i].trainable = False
-            print('Freeze the first {} layers of total {} layers.'.format(num, len(model_body.layers)))
-
-    model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
-                        arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.7})(
-        [*model_body.output, *y_true])
-    model = Model([model_body.input, *y_true], model_loss)
-
-    return model
-
-
 def model_body(input_shape, anchors, num_classes, update_callback, load_pretrained=True, freeze_body=0,
-               weights_path='model_data/yolo_weights.h5'):
+               init_weights='model_data/yolo_weights.h5', last_save='model_data/tfmodel'):
     K.clear_session()  # get a new session
     image_input = Input(shape=(None, None, 3))
     h, w = input_shape
@@ -226,10 +163,11 @@ def model_body(input_shape, anchors, num_classes, update_callback, load_pretrain
 
     model_body = yolo_body(image_input, num_anchors // 3, num_classes)
     print('Create YOLOv3 model with {} anchors and {} classes.'.format(num_anchors, num_classes))
-
-    if load_pretrained:
-        model_body.load_weights(weights_path, by_name=True)
-        print('Load weights {}.'.format(weights_path))
+    if os.path.exists(last_save):
+        tf.saved_model.load(last_save)
+    elif load_pretrained:
+        model_body.load_weights(init_weights, by_name=True)
+        print('Load weights {}.'.format(init_weights))
         if freeze_body in [1, 2]:
             # Freeze darknet53 body or freeze all but 3 output layers.
             num = (185, len(model_body.layers) - 3)[freeze_body - 1]
